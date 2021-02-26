9122e6874fd8b2b6fa7598e71604d5180052cd2e
pschlief <pschlief@awi.de>
2021-02-26 19:58:24 +0100

    added variables program

---
+++ variables/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	//var publisher string
+	var writer string
+	var artist string
+	var title string
+	//var year int
+	//var pageNumber int
+	//var grade float32
+
+	//publisher := "DizzyBooks Publishing Inc"
+	writer = "Tracey Hatchet"
+	artist = "Jewel Tampson"
+	title = "Mr. GoToSleep"
+	//year := 1997
+	//pageNumber := 14
+	//grade := 6.5
+
+	fmt.Println(title, " written by ", writer, ". Drawed by ", artist)
+
+	//publisher = "DizzyBooks Publishing Inc"
+	writer = "Ryan N"
+	artist = "Phoebe Paperclips"
+	title = "Epic Vol. 1"
+	//year = 2013
+	//pageNumber = 160
+	//grade := 9.0
+
+	fmt.Println(title, " written by ", writer, ". Drawed by ", artist)
+
+}
